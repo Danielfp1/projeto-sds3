13f3147776d76e8c275d554106621bd29bbf85b3
Daniel Figueiredo Pereira <danielfp.pc@hotmail.com>
2021-05-07 17:34:05 -0300

    Pagination

---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,30 @@
+import { SalePage } from "types/sale";
+
+type Props = {
+    page: SalePage;
+    onPageChange: Function; // comunicação entre componentes
+}
+
+const Pagination = ({ page, onPageChange } : Props ) => {
+
+    return (
+        <div className="row d-flex justify-content-center">
+            <nav>
+                <ul className="pagination">
+                    {/* if para verificar se é a primeira pagina */}
+                    <li className={`page-item ${page.first ? 'disabled' : ''}`}> 
+                        <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
+                    </li>
+                    <li className="page-item disabled">
+                        <span className="page-link">{page.number + 1}</span>
+                    </li>
+                    <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Pagination;
